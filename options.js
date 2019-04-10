7e63033dc9720ec2e0a3a084a78f908f7ec675bd
smgehrig <smgehrig@comcast.net>
2019-04-10 01:29:33 -0400

    Added reference to css sheet and added ability to collapse details

--- options.js
+++ options.js
@@ -1,19 +1,9 @@
-
-
-
-
-/*let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-for (let item of kButtonColors) {
-  let button = document.createElement('button');
-  button.style.backgroundColor = item;
-  button.addEventListener('click', function() {
-	chrome.storage.sync.set({color: item}, function() {
-	  console.log('color is ' + item);
-	})
-  });
-  page.appendChild(button);
+function hideDetails(detailsToHide){
+	var x = document.getElementById(detailsToHide);
+	if(x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
 }
-}
-constructOptions(kButtonColors);*/
+	
